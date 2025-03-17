3bb42fe54b043a6d19c38309d5dbafd71278eff5
andressa-ricardo <andressaricardo30@gmail.com>
2025-03-17 17:40:55 -0300

    fix(layout): update default URL to use VERCEL_URL instead of DATABASE_URL

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
 
-const defaultUrl = process.env.DATABASE_URL
-  ? `https://${process.env.DATABASE_URL}`
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
 
 export const metadata = {
